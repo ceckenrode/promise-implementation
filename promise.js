0f0c99621576057e2a1a6e9511f17f9da885801f
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-10-26 23:10:51 -0400

    Initialized state, set up change state function

---
+++ promise.js
@@ -0,0 +1,39 @@
+var State = {
+ PENDING: 0,
+ FULFILLED: 1,
+ REJECTED: 2
+};
+
+var Aplus = {
+ state: State.PENDING,
+ changeState: function(state, value) {
+
+  // catch changing to same state (perhaps trying to change the value)
+  if ( this.state == state ) {
+   throw new Error("can't transition to same state: " + state);
+  }
+
+  // trying to change out of fulfilled or rejected
+  if ( this.state == State.FULFILLED ||
+    this.state == State.REJECTED ) {
+   throw new Error("can't transition from current state: " + state);
+  }
+
+  // if second argument isn't given at all (passing undefined allowed)
+  if ( state == State.FULFILLED &&
+    arguments.length < 2 ) {
+   throw new Error("transition to fulfilled must have a non null value");
+  }
+
+  // if a null reason is passed in
+  if ( state == State.REJECTED &&
+    value == null ) {
+   throw new Error("transition to rejected must have a non null reason");
+  }
+
+  //change state
+  this.state = state;
+  this.value = value;
+  return this.state;
+ }
+};
